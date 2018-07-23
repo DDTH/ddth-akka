0c64232073b74d70b1ba777703c56bb32f07be1b
Thanh Nguyen <btnguyen2k@gmail.com>
2018-07-23 22:37:25 +0700

    typo

--- ddth-akka-core/src/main/java/com/github/ddth/akka/scheduling/WorkerCoordinationPolicy.java
+++ ddth-akka-core/src/main/java/com/github/ddth/akka/scheduling/WorkerCoordinationPolicy.java
@@ -14,14 +14,14 @@ public enum WorkerCoordinationPolicy {
     TAKE_ALL_TASKS(0),
 
     /**
-     * On one node, worker can take one task as a time.But workers one two or
-     * more nodes can execute tasks simultaneously.
+     * On one node, worker can take only one task at a time. But workers on other
+     * nodes can execute tasks simultaneously.
      */
     LOCAL_SINGLETON(1),
 
     /**
      * Once worker takes a task, all of its instances on all nodes are marked
-     * "busy" and can not take any more task until free.
+     * "busy" and can no longer take any more task until free.
      */
     GLOBAL_SINGLETON(2);
 
